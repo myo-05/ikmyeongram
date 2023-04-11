d251d82b54194e719ace3d2cfbeff536293728e9
Young-Oh <gzf0547@gmail.com>
2023-04-11 17:18:42 +0900

    urls & admin setting

--- igmsns/sns/urls.py
+++ igmsns/sns/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('post/<int:id>/', views.detail_post_view),  # /api/sns/post/<int:id>/ 게시글 상세 페이지
     path('post/<int:id>/delete/', views.delete, name='delete'),# /api/post/{Post_id}/ 게시글 삭제
     path('post/<int:id>/update/', views.update, name='update'),  # /api/post/{Post_id}/ 게시글 수정 페이지
+    path('profile/<str:post_author>/', views.profile_view),  # /api/sns/profile/<int:id>/ 프로필페이지
 ]
