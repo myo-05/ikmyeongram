85c0a385881b36955631a9dcfbf07e7afaee7eab
Chaeyeon Seo <ssallyseo@gmail.com>
2023-04-11 21:57:10 +0900

    이미지업로드

--- igmsns/sns/views.py
+++ igmsns/sns/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Post
 
-
-# Create your views here.
+# @login_required(login_url='/sign-in') # 로그인을 하지 않고 url을 통해 접속할 경우 리디렉션
 
 '''
 정은님이 만드실 홈화면 구동을 위해 임시로 만들었습니다 뷰 함수 이름만 다르고 완전히 같으니까 참고해서 merge 해 주시면 됩니다 팀장님 ^-^
@@ -18,15 +17,21 @@ def home(request): # 홈 화면으로 렌더링
 def new(request): # 새 글 작성 페이지로 렌더링
     return render(request, 'sns/new_post.html')
     
-# @login_required(login_url='/sign-in') # 로그인을 하지 않고 url을 통해 접속할 경우 리디렉션
+    
+# 글 작성
+
 def new_post_view(request):
     if request.method == 'POST':
         post_title = request.POST['post_title']
         post_content = request.POST['post_content']
         # post_author = 유저id
-        post = Post.objects.create(post_title=post_title, post_content=post_content)
+        post_img = request.FILES.get('post_img') # 이미지 업로드 받아오기
+        
+        post = Post.objects.create(post_title=post_title, post_content=post_content, post_img=post_img)
         post.save()
+        
         return redirect('home')
+    
     return render(request, 'new_post.html')
         
          
