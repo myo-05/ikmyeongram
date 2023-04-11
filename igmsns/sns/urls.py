a59d4952a99ce147767ec6d939a9c626f58dee75
Chaeyeon Seo <ssallyseo@gmail.com>
2023-04-11 17:14:23 +0900

    add posting

---
+++ igmsns/sns/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from sns import views
+
+urlpatterns = [
+    path('', views.home, name='home'),  # /api/sns/
+    path('new/', views.new, name='new'), # /api/sns/new
+    path('post/', views.new_post_view, name='post'),  # /api/sns/post/
+
+
+]
