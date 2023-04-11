afb846afe9283dd379bf9dd7d9d678c4e59be8f6
chanjulee <crdlckswn@naver.com>
2023-04-11 21:17:26 +0900

    user 데이터베이스 생성, user 데이터베이스 =my_user, 템플릿에 user 폴더 생성, user 폴더 안 (signin, signup) html 생성

--- igmsns/igmsns/urls.py
+++ igmsns/igmsns/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/user/", include("user.urls")),
     path("api/sns/", include("sns.urls")),
+    path('', include('user.urls')),
 ]
--- igmsns/sns/urls.py
+++ igmsns/sns/urls.py
@@ -3,8 +3,6 @@ from sns import views
 
 urlpatterns = [
     path('', views.home, name='home'),  # /api/sns/
-    path('new/', views.new, name='new'), # /api/sns/new
+    path('new/', views.new, name='new'),  # /api/sns/new
     path('post/', views.new_post_view, name='post'),  # /api/sns/post/
-
-
 ]
