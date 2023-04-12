602fc30ad881c46232eea2aca8872ceef4179706
chanjulee <crdlckswn@naver.com>
2023-04-12 15:37:36 +0900

    회원가입 기능 추가

--- igmsns/user/views.py
+++ igmsns/user/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import UserModel
 
 # Create your views here.
+
+
 def sign_up_view(request):
-    return render(request, 'user/signup.html')
+    if request.method == 'GET':
+        return render(request, 'user/signup.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        password2 = request.POST.get('password2', None)
+        bio = request.POST.get('bio', None)
+
+        if password != password2:
+            return render(request, 'user/signup.html')
+        else:
+            new_user = UserModel()
+            new_user.username = username
+            new_user.password = password
+            new_user.bio = bio
+            new_user.save()
+
+        return redirect('/api/user/sign-in')
 
 
 def sign_in_view(request):
