bbda9a287e36d3b8f19c01c99e9087d9423cd433
ark375@naver.com <ark375@naver.com>
2023-04-13 01:39:18 +0900

    haha

--- igmsns/user/urls.py
+++ igmsns/user/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('sign-up/', views.sign_up_view, name='sign-up'),
+    path('sign-up/', views.sign_up_view),
     path('sign-in/', views.sign_in_view, name='sign-in'),
     path('sign-out/', views.logout, name = 'sign-out'),
-    path('sign-up-detail/', views.sign_up_detail), # 정은 : 임시로 만들었습니다
+    path('sign-up-detail/', views.sign_up_detail,name='sign-up'), # 정은 : 임시로 만들었습니다
 ]
