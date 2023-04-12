d7ccc650b5ec14b56bdcf7118a4002103ce0347a
chanjulee <crdlckswn@naver.com>
2023-04-12 16:45:25 +0900

    데이터베이스에 데이터 들어감, 비밀번호 암호화 기능추가

--- igmsns/user/views.py
+++ igmsns/user/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import UserModel
+from django.http import HttpResponse
+from django.contrib.auth import get_user_model
+
 
 # Create your views here.
 
@@ -16,14 +19,26 @@ def sign_up_view(request):
         if password != password2:
             return render(request, 'user/signup.html')
         else:
-            new_user = UserModel()
-            new_user.username = username
-            new_user.password = password
-            new_user.bio = bio
-            new_user.save()
-
-        return redirect('/api/user/sign-in')
+            exist_user = get_user_model().objects.filter(username=username)
+            if exist_user:
+                return render(request, 'user/signup.html')
+            else:
+                UserModel.objects.create_user(
+                    username=username, password=password, bio=bio)
+                return redirect('/api/user/sign-in')
 
 
 def sign_in_view(request):
-    return render(request, 'user/signin.html')
+    if request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+
+        me = UserModel.objects.get(username=username)
+        if me.password == password:
+            request.session['user'] = me.username
+            return HttpResponse("로그인 성공!")
+        else:
+            return redirect('/api/user/sign-in')
+
+    elif request.method == 'GET':
+        return render(request, 'user/signin.html')
