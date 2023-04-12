72dcc92927b6a5f9bef042ab56354065623f8deb
Young-Oh <gzf0547@gmail.com>
2023-04-13 03:15:07 +0900

    restart

--- igmsns/sns/models.py
+++ igmsns/sns/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from user.models import UserModel
 
 # 게시글 테이블 모델
 class Post(models.Model):
     class Meta:
         db_table = "posting"
         
+    author = models.ForeignKey(UserModel, on_delete=models.CASCADE)
     post_author = models.CharField("작성자", max_length=256) # user 모델의 사용자를 상속받습니다. 추후에 바뀌어야 합니다.
     post_title = models.CharField("글제목", max_length=45)
     post_content = models.TextField("글내용")
