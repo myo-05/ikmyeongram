d4d272b2687683e985b9f3ce855a6e54aaf5100d
Young-Oh <gzf0547@gmail.com>
2023-04-13 19:04:53 +0900

    error message setting

--- igmsns/user/views.py
+++ igmsns/user/views.py
@@ -27,21 +27,25 @@ def sign_up_detail(request):
         nickname = request.POST.get('nickname', None)  # 닉네임
         email = request.POST.get('email', None)  # 이메일
         user_img = request.FILES.get('user_img')  # 이미지 업로드 받아오기
-
-        if password != password2:
-            return render(request, 'user/signup_detail.html')
-        else:
-            exist_user = get_user_model().objects.filter(username=username)
-            if exist_user:
-                # 중복된 ID라면 회원가입창으로 돌아가기
-                # 영오: 이미 있는 ID입니다. 새로 지정해주세요. 메세지 떠야합니다.
-                return render(request, 'user/signup_detail.html', {'error_message': '이미 있는 ID입니다. 새로 지정해주세요.'})
+        #공란이 하나라도 있으면 else가 작동하여 에러메세지 출력
+        if username and password and password2 and nickname and email :
+            if password != password2:
+                return render(request, 'user/signup_detail.html',{'error_message': '[비밀번호불일치!] 진정하고 천천히 다시써봐요 예?'})
             else:
-                # 유저계정생성하여 DB저장
-                # 영오: 회원가입완료. 메세지 떠야합니다.
-                UserModel.objects.create_user(
-                    username=username, password=password, nickname=nickname, email=email, user_img=user_img)
-                return redirect('sign-in')
+                exist_user = get_user_model().objects.filter(username=username)
+                if exist_user:
+                    # 중복된 ID라면 회원가입창으로 돌아가기
+                    # 영오: 이미 있는 ID입니다. 새로 지정해주세요. 메세지 떠야합니다.
+                    return render(request, 'user/signup_detail.html', {'error_message': '[중복 발견!] 이미 있는 ID입니다. 아시겠어요?'})
+                else:
+                    # 유저계정생성하여 DB저장
+                    # 영오: 회원가입완료. 메세지 떠야합니다.
+                    UserModel.objects.create_user(
+                        username=username, password=password, nickname=nickname, email=email, user_img=user_img)
+                    return redirect('sign-in')
+        else:    
+            return render(request, 'user/signup_detail.html',{'error_message': '[빈칸 발견!] 공란이 빤히 보이는데, 미치셨어요?'})
+        
 
 
 # 로그인 함수
@@ -50,8 +54,7 @@ def sign_in_view(request):
         username = request.POST.get('username', None)  # ID 받아옴
         password = request.POST.get('password', None)  # 비밀번호 받아옴
 
-        me = auth.authenticate(request, username=username,
-                               password=password)  # 유저 인증(회원가입 된 유저인지)
+        me = auth.authenticate(request, username=username,password=password)  # 유저 인증(회원가입 된 유저인지)
         if me is not None:  # 계정이 있다면
             auth.login(request, me)
             return redirect('home')
