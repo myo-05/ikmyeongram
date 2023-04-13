bd78e385bb56c6e8b94f00772f2ecdfba8c52255
Chaeyeon Seo <ssallyseo@gmail.com>
2023-04-13 19:01:24 +0900

    modify thumbnail function

--- igmsns/sns/models.py
+++ igmsns/sns/models.py
@@ -5,13 +5,11 @@ from django.db import models
 class Post(models.Model):
     class Meta:
         db_table = "posting"
-    # user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts') #유저의 아이디
     
-    post_author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts') # 유저 모델의 사용자를 상속받습니다.
-    
+    post_author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts') # 글 작성자 : 유저 모델의 사용자를 상속받습니다.
     post_title = models.CharField("글제목", max_length=45)
     post_content = models.TextField("글내용")
-    post_img = models.FileField("이미지", upload_to='',blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    author_id = models.CharField(max_length=45)
+    post_img = models.FileField("이미지", upload_to='',blank=True, null=True) # 글 내 이미지 업로드
+    created_at = models.DateTimeField(auto_now_add=True) # 생성시각
+    updated_at = models.DateTimeField(auto_now=True) # 수정시각
+    author_id = models.CharField(max_length=45) # 현재 로그인 중인 유저의 기본키 넣을 자리
