e129891642f9e0acaa25912d7502a84dfa2895b9
Chaeyeon Seo <ssallyseo@gmail.com>
2023-04-14 01:06:20 +0900

    add comment

--- igmsns/sns/urls.py
+++ igmsns/sns/urls.py
@@ -5,8 +5,12 @@ urlpatterns = [
     path('', views.home, name='home'),  # /api/sns/
     path('new/', views.new, name='new'), # /api/sns/new
     path('post/', views.new_post_view, name='post'),  # /api/sns/post/
-    path('post/<int:id>/', views.detail_post_view, name = 'detail'),  # /api/sns/post/<int:id>/ 게시글 상세 페이지
+    path('post/<int:id>/', views.detail_post_view, name = 'detail'),  # /api/sns/post/<id>/ 게시글 상세 페이지
     path('post/<int:id>/delete/', views.delete, name='delete'),# /api/post/{Post_id}/ 게시글 삭제
     path('post/<int:id>/update/', views.update, name='update'),  # /api/post/{Post_id}/ 게시글 수정 페이지
-    path('profile/<str:post_author>/', views.profile_view),  # /api/sns/profile/<int:id>/ 프로필페이지
+        
+    path('post/<int:id>/comment/', views.comment_create, name='comment'),  # /api/sns/post/<id>/comment/ 댓글 작성하기
+    path('post/<int:id>/comment/<int:comment_id>/delete/', views.comment_delete, name='comment_delete'),  # /api/sns/post/<id>/comment/<comment_id>/delete 댓글 삭제하기
+    
+    path('profile/<str:post_author>/', views.profile_view),  # /api/sns/profile/<id>/ 프로필페이지
 ]
